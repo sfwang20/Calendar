85a6ca49131b897cc601c1b183231bbaf93f9d45
Shin-Fu <shinfu619@gmail.com>
2020-04-16 09:30:55 +0800

    use include to tear down files footer and head

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Calendar</title>
+
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/main.css">
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/action.js"></script>
+    <script src="js/handlebars-v4.7.3.js"></script>
+  </head>
+  <body>
