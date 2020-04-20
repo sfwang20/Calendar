e8478a6043cee21d7a56e222ee7427ae7208d450
Shin-Fu <shinfu619@gmail.com>
2020-04-20 11:18:02 +0800

    adjust lines for improving readadility

--- js/action.js
+++ js/action.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){    
+$(document).ready(function(){
 
     var source = $('#event-template').html();
-    var eventTemplate = Handlebars.compile(source);   
+    var eventTemplate = Handlebars.compile(source);
 
     $.each(events, function(index, event){
         var eventUI = eventTemplate(event) ;
         var date = event['date'];
         $('#calendar').find('.date-block[data-date="' + date +'"]').find('.events').append(eventUI);
     });
-       
+
     var panel = {
         el: '#info-panel',
         selectedDateBlock: null,
@@ -34,7 +34,7 @@ $(document).ready(function(){
             panel.init(isNew, e);
             panel.hideError();
             $(panel.el).addClass('open').css({       //panel.el等同#info-panel
-                top: e.pageY+'px', 
+                top: e.pageY+'px',
                 left: e.pageX+'px',
             }).find('.title [type]').focus();   //[]是找屬性
         },
@@ -45,16 +45,16 @@ $(document).ready(function(){
             //get date from .data-block
             if ($(e.currentTarget).is('.date-block'))          //如果點到的是data-block
                 var date = $(e.currentTarget).data('date');
-            else 
+            else
                 var date = $(e.currentTarget).closest('.date-block').data('date');
             //get month from calendar
             var year = $('#calendar').data('year');
             var month = $('#calendar').data('month');
-            
+
             $(panel.el).find('.month').text(month);             //panel介面顯示正確的月份和日期
             $(panel.el).find('.date').text(date);
 
-            $(panel.el).find('[name="year"]').val(year);    
+            $(panel.el).find('[name="year"]').val(year);
             $(panel.el).find('[name="month"]').val(month);     //塞值/更新到input裡(存到form裡) 要用console.log來確認是否成功
             $(panel.el).find('[name="date"]').val(date);        //注意[]寫法
         },
@@ -73,7 +73,7 @@ $(document).ready(function(){
     }).on('dblclick', '.event', function(e){
         e.stopPropagation();       //防止事件向父層傳遞 網頁的事件預設會一直往上傳播 date-block也有綁dblclick所以會觸發
         panel.open(false, e);     //開update的
-        
+
         panel.selectedEvent = $(e.currentTarget);  //記錄點到的event, for delete用
 
         var id = $(this).data('id');
@@ -95,7 +95,7 @@ $(document).ready(function(){
     $(panel.el)
     .on('click', 'button', function(e){
        if ($(this).is('.create') || $(this).is('.update')){
-           if ($(this).is('.create')) { 
+           if ($(this).is('.create')) {
                 var action = 'event/create.php';
            }
            if ($(this).is('.update')) {
@@ -103,19 +103,19 @@ $(document).ready(function(){
            }
         //collect data
         var data = $(panel.el).find('form').serialize();   //一坨字串 網址?後面的 一次蒐集所有input資料
-        console.log(data);
+        
         $.post(action, data)
             .done(function (data, textStatus, jqXHR) {
                 if ($(e.currentTarget).is('.update'))
                     panel.selectedEvent.remove();
 
                 var eventUI = eventTemplate(data);    //生成完之後要插回去 插在哪? 用selectedDateBlock記錄當初點的是誰
-                
+
                 panel.selectedDateBlock.find('.event').each(function(index, event){
                     var eventFromTime = $(event).data('from').split(':');
                     var newEventFromTime = data.start_time.split(':');
-                    
-                    if (eventFromTime[0]>newEventFromTime[0] || 
+
+                    if (eventFromTime[0]>newEventFromTime[0] ||
                         (eventFromTime[0]==newEventFromTime[0] && eventFromTime[1]>newEventFromTime[1])) {
                         $(event).before(eventUI);
                         return false;  //break的意思 若要continue 寫return
@@ -123,8 +123,8 @@ $(document).ready(function(){
                 });
 
                 if (panel.selectedDateBlock.find('.event[data-id="'+data.id+'"]').length == 0)
-                    panel.selectedDateBlock.find('.events').append(eventUI);  
-                
+                    panel.selectedDateBlock.find('.events').append(eventUI);
+
                 panel.close();
             })
             .fail(function(xhr, textStatus, errorThrown){
