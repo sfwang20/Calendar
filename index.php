86e720444737a16589444222587ce4b8f81bc962
Shin-Fu <shinfu619@gmail.com>
2020-04-20 11:15:48 +0800

    remove unnecessary code

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php include ('header.php') ?>
 <?php include ('data.php') ?>
 <?php include ('template.php') ?>
-<?php 
-session_start();
+<?php
 if ($_SESSION["loggedin"] == false){
   header("location: register.php");
   exit;
@@ -37,44 +36,42 @@ if ($_SESSION["loggedin"] == false){
 
 <div id="info-panel" class="new">
     <div class="close">X</div>
-<form>
-    <input type="hidden" name="id">
-    <div class="title">
-        <label>event</label><br>
-        <input type="text" name="title">
-    </div>
-    <div class="error-msg">
-        <div class="alert alert-danger">error</div>
-    </div>
-    <div class="time-picker">
-        <div class="select-date">
-            <span class="month"></span>/<span class="date"></span>
-            <input type="hidden" name="year">
-            <input type="hidden" name="month">                 <!--非user輸入的 由程式決定 -->
-            <input type="hidden" name="date">
-        </div>
-        <div class="from">
-            <label for="from">from</label><br>
-            <input type="time" id="from" name="start_time">
-        </div>
-        <div class="to">
-            <label for="to">to</label><br>
-            <input type="time" id="to" name="end_time">
-        </div>
-    </div>
-    <div class="description">
-        <label>description</label><br>
-        <textarea name="description" id="description"></textarea>
-    </div>
-</form>
-    <div class="buttons clearfix" >
-            <button class="create">create</button>
-            <button class="update">update</button>
-            <button class="cancel">cancel</button>
-            <button class="delete">delete</button>
-    </div>
+  <form>
+      <input type="hidden" name="id">
+      <div class="title">
+          <label>event</label><br>
+          <input type="text" name="title">
+      </div>
+      <div class="error-msg">
+          <div class="alert alert-danger">error</div>
+      </div>
+      <div class="time-picker">
+          <div class="select-date">
+              <span class="month"></span>/<span class="date"></span>
+              <input type="hidden" name="year">
+              <input type="hidden" name="month">                 <!--非user輸入的 由程式決定 -->
+              <input type="hidden" name="date">
+          </div>
+          <div class="from">
+              <label for="from">from</label><br>
+              <input type="time" id="from" name="start_time">
+          </div>
+          <div class="to">
+              <label for="to">to</label><br>
+              <input type="time" id="to" name="end_time">
+          </div>
+      </div>
+      <div class="description">
+          <label>description</label><br>
+          <textarea name="description" id="description"></textarea>
+      </div>
+  </form>
+  <div class="buttons clearfix" >
+          <button class="create">create</button>
+          <button class="update">update</button>
+          <button class="cancel">cancel</button>
+          <button class="delete">delete</button>
+  </div>
 </div>
 
 <?php include ('footer.php') ?>
-
-
